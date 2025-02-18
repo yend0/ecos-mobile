5e49f957897d22b5caedaae00bfa877d69e2d279
Vadim Remezov <aspowersan@protonmail.com>
2025-02-19 01:20:43 +0300

    add root page for routing

--- ecos/lib/ecos_app.dart
+++ ecos/lib/ecos_app.dart
@@ -1,19 +1,17 @@
-
-import 'package:ecos/theme/theme.dart';
+import 'package:ecos/router/router.dart';
+import 'package:ecos/ui/core/themes/themes.dart';
 import 'package:flutter/material.dart';
-
-import 'features/home/home.dart';
 
 class EcosApp extends StatelessWidget {
   const EcosApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'Ecos',
-      theme: EcosAppTheme.lightTheme,
-      home: HomePage(),
+      theme: LightTheme.lightTheme,
+      routerConfig: router,
     );
   }
 }
---
+++ ecos/lib/features/root/view/root_page.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class RootPage extends StatelessWidget {
+  const RootPage({
+    super.key,
+    required this.navigationShell,
+  });
+
+  static const String _homeIcon = 'assets/icons/ic_home.svg';
+  static const String _profileIcon = 'assets/icons/ic_profile.svg';
+
+  final StatefulNavigationShell navigationShell;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Scaffold(
+      body: navigationShell,
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        selectedFontSize: 12,
+        unselectedFontSize: 12,
+        selectedLabelStyle: theme.textTheme.bodySmall,
+        unselectedLabelStyle: theme.textTheme.bodySmall,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black,
+        currentIndex: navigationShell.currentIndex,
+        items: _buildBottomNavBarItems(theme),
+        onTap: (index) => navigationShell.goBranch(
+          index,
+          initialLocation: index == navigationShell.currentIndex,
+        ),
+      ),
+    );
+  }
+
+  List<BottomNavigationBarItem> _buildBottomNavBarItems(ThemeData theme) => [
+        BottomNavigationBarItem(
+          icon: SvgPicture.asset(
+            RootPage._homeIcon,
+            height: 24,
+            width: 24,
+            colorFilter: ColorFilter.mode(theme.disabledColor, BlendMode.srcIn),
+          ),
+          activeIcon: SvgPicture.asset(
+            RootPage._homeIcon,
+            height: 24,
+            width: 24,
+            colorFilter: ColorFilter.mode(Colors.black, BlendMode.srcIn),
+          ),
+          label: 'Главная',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.recycling_rounded,
+            size: 24,
+            color: theme.disabledColor,
+          ),
+          activeIcon: Icon(
+            Icons.recycling_rounded,
+            size: 24,
+          ),
+          label: 'Переработка',
+        ),
+        BottomNavigationBarItem(
+          icon: SvgPicture.asset(
+            RootPage._profileIcon,
+            height: 24,
+            width: 24,
+            colorFilter: ColorFilter.mode(theme.disabledColor, BlendMode.srcIn),
+          ),
+          activeIcon: SvgPicture.asset(
+            RootPage._profileIcon,
+            height: 24,
+            width: 24,
+            colorFilter: ColorFilter.mode(Colors.black, BlendMode.srcIn),
+          ),
+          label: 'Профиль',
+        ),
+      ];
+}
