cd23384b667a8a8843f8bba2747fc02aaf5ad802
Vadim Remezov <aspowersan@protonmail.com>
2025-02-19 01:19:55 +0300

    add routing

---
+++ ecos/lib/router/app_router.dart
@@ -0,0 +1,63 @@
+import 'package:go_router/go_router.dart';
+
+import 'package:ecos/features/home/home.dart';
+import 'package:ecos/features/profile/profile.dart';
+import 'package:ecos/features/recycle/recycle.dart';
+import 'package:ecos/features/root/root.dart';
+
+final router = GoRouter(
+  initialLocation: '/home',
+  routes: [
+    // BottomNavigationBar
+    StatefulShellRoute.indexedStack(
+      builder: (context, state, navigationShell) =>
+          RootPage(navigationShell: navigationShell),
+      branches: [
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: '/home',
+              builder: (context, state) => const HomePage(),
+            )
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: '/recycle',
+              builder: (context, state) => const RecyclePage(),
+            )
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: '/profile',
+              builder: (context, state) => const ProfilePage(),
+              routes: [
+                GoRoute(
+                  path: 'account',
+                  builder: (context, state) => const AccountPage(),
+                ),
+                GoRoute(
+                  path: 'history',
+                  builder: (context, state) => const HistoryPage(),
+                ),
+                GoRoute(
+                  path: 'knowledge-base',
+                  builder: (context, state) => const KnowledgeBasePage(),
+                  routes: [
+                    GoRoute(
+                      path: 'knowledge-detail',
+                      builder: (context, state) => const KnowledgeDetailPage(),
+                    ),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
+      ],
+    ),
+  ],
+);
