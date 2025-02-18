fb9c8560bb4cd69cf02b1f3618249e7384c2cecc
Vadim Remezov <aspowersan@protonmail.com>
2025-02-19 01:21:42 +0300

    add recycle page

---
+++ ecos/lib/features/recycle/view/recycle_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RecyclePage extends StatelessWidget {
+  const RecyclePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
